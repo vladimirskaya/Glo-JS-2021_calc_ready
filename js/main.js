df62d5e8bde58c619324ce22b662d425c567e772
Nina Zurbulaeva <naina148@mail.ru>
2021-01-30 04:29:48 +0300

    feat: add comments | variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
-let money, income, addExpenses, deposit, mission, period;
+let money, income, addExpenses, deposit; /* переменные, связанные с деньгами */
+let mission, period; /* переменные по назначению и периодам */
 alert("Сообщение с любым текстом");
 console.log("Сообщение с любым текстом в консоли");
