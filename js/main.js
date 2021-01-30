e01c6991266a1f51b0918ff8eeee654cf9289eb4
Nina Zurbulaeva <naina148@mail.ru>
2021-01-30 05:17:20 +0300

    feat: add new variable  |

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 let money, income, addExpenses, deposit; /* переменные по доходам и расходам */
 let mission, period; /* переменные: цель и за какой период */
+let budgetDay;
 
-money = 15000; 
+money = 45000;  // Доход за месяц
 income = "фриланс";
 addExpenses = "Интернет, Такси, Коммуналка, кОфе, Вкукусняхи и прочая ересь";
 deposit = true;
 mission = 1000000;
 period = 12;
+budgetDay = money/30;
 
 console.log(`Тип переменной money: ${typeof money}`);
 console.log(`Тип переменной income: ${typeof income}`);
@@ -15,3 +17,4 @@ console.log(`Длина строки addExpenses: ${addExpenses.length}`);
 console.log(`Период равен: ${period} месяцев`);
 console.log(`Цель: заработать ${mission} рублей`);
 console.log("addExpenses", addExpenses.toLowerCase().split(','));
+console.log(`Дневной бюджет: ${budgetDay}`);
