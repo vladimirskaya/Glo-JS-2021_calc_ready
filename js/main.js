916cc04c4bfbf67d86d3fa5aa903eeb66ae8f0cc
Nina Zurbulaeva <naina148@mail.ru>
2021-01-30 04:51:25 +0300

    feat: insert values | variables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-let money, income, addExpenses, deposit; /* переменные, связанные с деньгами */
-let mission, period; /* переменные по назначению и периодам */
+let money, income, addExpenses, deposit; /* переменные по доходам и расходам */
+let mission, period; /* переменные: цель и за какой период */
+
+money = 15000; 
+income = "фриланс";
+addExpenses = "интернет, такси, коммуналка, кофе, вкусняхи и прочая ересь";
+deposit = true;
+mission = 1000000;
+period = 12;
 
 alert("Сообщение с любым текстом");
 console.log("Сообщение с любым текстом в консоли");
