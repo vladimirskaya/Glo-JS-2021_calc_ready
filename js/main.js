7733083648dff505658765625df46d650de16fd1
Nina Zurbulaeva <naina148@mail.ru>
2021-02-02 21:42:19 +0300

    feat: change orger of variable declaration |

--- js/main.js
+++ js/main.js
@@ -1,35 +1,25 @@
 'use strict'
 
-let money = 45000;  // Доход за месяц
+let money = +prompt("Ваш месячный доход?");
 let income = "фриланс";
-let addExpenses = "Интернет, Такси, Коммуналка, кОфе, Вкукусняхи и прочая ересь";
-let deposit = true;
+let addExpenses = (prompt("Перечислите возможные расходы за рассчитываемый период через запятую")).split(',');
+let deposit = confirm('Есть ли у вас депозит в банке?');    
 let mission = 1000000;
 let period = 12;
 let budgetMonth, budgetDay; /* переменные бюджета по перидам */
-let exp1, exp2, cost1, cost2;  /* дополнитеьные статьи расходов */
+// Дополнительные статьи расходов
+let exp1 = prompt("Введите обязательную статью расходов?");
+let cost1 = prompt("Во сколько это обойдется?");
+let exp2 = prompt("Введите обязательную статью расходов?");
+let cost2 = prompt("Во сколько это обойдется?");
 
-console.log(`Тип переменной money: ${typeof money}`);
+/*console.log(`Тип переменной money: ${typeof money}`);
 console.log(`Тип переменной income: ${typeof income}`);
 console.log(`Тип переменной deposit: ${typeof deposit}`);
 console.log(`Длина строки addExpenses: ${addExpenses.length}`);
 console.log(`Период равен: ${period} месяцев`);
 console.log(`Цель: заработать ${mission} рублей`);
-console.log("addExpenses", addExpenses.toLowerCase().split(','));
-
-//Получение данных от пользователя
-money = +prompt("Ваш месячный доход?");
-addExpenses = (prompt("Перечислите возможные расходы за рассчитываемый период через запятую")).split(',');
-console.log(addExpenses);
-
-// Присвоение булевого значения в соответствии с ответом от пользователя
-deposit = confirm('Есть ли у вас депозит в банке?');                        
-
-// Дополнительные статьи расходов
-exp1 = prompt("Введите обязательную статью расходов?");
-cost1 = prompt("Во сколько это обойдется?");
-exp2 = prompt("Введите обязательную статью расходов?");
-cost2 = prompt("Во сколько это обойдется?");
+console.log("addExpenses", addExpenses.toLowerCase().split(','));*/
 
 // Расчет бюджета по периодам, дополнительно нахождение количества месяцев для достижения заданной цели
 budgetMonth = money - cost1 - cost2;
