ebcf0bcbcdd57ef13471e32e1c8735efb74ac76e
Nina Zurbulaeva <naina148@mail.ru>
2021-02-03 00:18:36 +0300

    feat: внесла изменения согласно обратной связи |

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 let money = +prompt("Ваш месячный доход?");
 let income = "фриланс";
-let addExpenses = (prompt("Перечислите возможные расходы за рассчитываемый период через запятую")).split(',');
+let addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую");
 let deposit = confirm('Есть ли у вас депозит в банке?');    
 let mission = 1000000;
-let period = 12;
+const period = 12;
 let budgetMonth, budgetDay; /* переменные бюджета по перидам */
 // Дополнительные статьи расходов
 let exp1 = prompt("Введите обязательную статью расходов?");
@@ -13,19 +13,18 @@ let cost1 = prompt("Во сколько это обойдется?");
 let exp2 = prompt("Введите обязательную статью расходов?");
 let cost2 = prompt("Во сколько это обойдется?");
 
-/*console.log(`Тип переменной money: ${typeof money}`);
+console.log(`Тип переменной money: ${typeof money}`);
 console.log(`Тип переменной income: ${typeof income}`);
 console.log(`Тип переменной deposit: ${typeof deposit}`);
 console.log(`Длина строки addExpenses: ${addExpenses.length}`);
 console.log(`Период равен: ${period} месяцев`);
 console.log(`Цель: заработать ${mission} рублей`);
-console.log("addExpenses", addExpenses.toLowerCase().split(','));*/
+console.log(addExpenses.toLowerCase().split(', '));*/
 
 // Расчет бюджета по периодам, дополнительно нахождение количества месяцев для достижения заданной цели
 budgetMonth = money - cost1 - cost2;
-console.log("Бюджет на конец месяца:", budgetMonth );
-period = Math.ceil(mission / budgetMonth);
-console.log(`Цель будет достигнута за ${period} месяцев(-а)`);
+console.log("Бюджет на месяц:", budgetMonth );
+console.log(`Цель будет достигнута за ${Math.ceil(mission / budgetMonth)} месяцев(-а)`);
 budgetDay = Math.floor(budgetMonth / 30);
 console.log("Бюджет на день: ", budgetDay);
 
